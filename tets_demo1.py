a8880225c820e88c47cac2ec4a61fdf80501f4a7
monica <huangxiu.ping@foxmail.com>
2022-04-19 14:10:10 +0800

    update

--- tets_demo1.py
+++
@@ -1,6 +0,0 @@
-from pygame import mixer
-
-mixer.init() # initiate the mixer instance
-mixer.music.load(r'D:\AR_Glass_TestToolkit\start.mp3') # loads the music, can be also mp3 file.
-mixer.music.play() # plays the music
-mixer.music.stop()
